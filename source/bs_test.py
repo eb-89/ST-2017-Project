bd2e021b86a0b433d346a01cc5e40fc72c744a4a
Erik Bertse <erik_bertse@hotmail.com>
2017-11-19 20:19:37 +0100

    Initial Commit - Software Testing Project HT2017

---
+++ source/bs_test.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+import elements as el
+
+#The html file
+html_doc = """
+<html>
+<head>
+	<title>The Dormouse's story</title>
+</head>
+
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+
+
+# a new page element, this class contains _find_all and attributeChecker
+pe = el.PageElement()
+
+# needs to run for initalization of the PE.
+pe.setup()
+
+# How to use the tag constructor, the name attribute must always be defined.
+tag1 = el.Tag(pe, attrs = (("a", "valueofa"), ("b", "valueofb")), name="name1")
+tag2 = el.Tag(pe, attrs = ({"notA": "valueofnotA", "notB": "valueofnotB"}), name="name2")
+tag3 = el.Tag(pe, attrs = ({"notA": "valueofnotA"}), name="name2")
+
+
+print(pe.format_string("test_string_0"))
+
+ #format_sting always runs with this attr.
+print(pe.format_string( "test_string_0", formatter="minimal"))
+
+
+# _attribute_checker returns a lambda function (https://en.wikipedia.org/wiki/Anonymous_function)
+ac = pe._attribute_checker("=", "a", value="valueofa") #true if this attribute is there, otherwise false
+ac2 = pe._attribute_checker("=", "notB", value="valueofnotB") #true if this attribute is there, otherwise false
+
+
+print(tag1.get("a")) ##prints "valueofa"
+
+print(ac(tag1)) #tag 1 contains attribute "a = valueofa", returns true
+print(ac(tag2)) #tag 2 doesnt, returns false
+print(ac2(tag2)) #True
+print(ac2(tag3)) #False
+
+##this calls _find_all within the pageElement object, see Tag constructor.
+# I haven't found a way to call that func directly, in a sane way, yet.
+tag1.find_all("test_string_2") 
+
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+#print(soup.find(class = 'sister'))
+#print(soup.find(id = 'link1'))
